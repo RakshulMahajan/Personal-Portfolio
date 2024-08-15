da13c56ae30ade0bf83cc4fd4de8d3a47eae8484
Rakshul Mahajan <rakshulmahajan@gmail.com>
2024-08-15 10:27:39 +0530

    first

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,105 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
+  "imagebaseurl": "https://rbhatia46.github.io/",
+  "name": "Rakshul Mahajan",
+  "role": "Web Developer",
+  "linkedinId": " http://linkedin.com/in/rakshul-mahajan-4a036021b",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "http://linkedin.com/in/rakshul-mahajan-4a036021b",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/RakshulMahajan",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "http://twitter.com/",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "I am currently a final year student at Chandigarh University pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into DSA. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  "address": "rakshulmahajan@gmail.com",
+  "education": [
+    {
+      "UniversityName": "Chandigarh University (CSE)",
+      "MonthOfPassing": "June",
+      "YearOfPassing": "2021 - June 2025",
+      "Achievements": "8 CGPA"
+    },
+    {
+      "UniversityName": "Kartar Public School (12th)",
+      "MonthOfPassing": "May",
+      "YearOfPassing": "2020 - June 2025",
+      "Achievements": "94.2 %"
+    },
+    
+  ],
+  "work": [
+    {
+      "CompanyName": "Dabotics Inidia Pvt. Ltd.",
+      "specialization": "Web Development",
+      "MonthOfLeaving": "June",
+      "YearOfLeaving": "2023 - July 2023",
+      "Achievements": "I was assigned different projects every week, which were mostly based on UI design and development."
+    },
+    {
+      "CompanyName": "Brainly",
+      "specialization": "Subject Matter Expert (CS)",
+      "MonthOfLeaving": "June",
+      "YearOfLeaving": "2022 - October 2022",
+      "Achievements": "I was working an a SME for the subject Computer Science, where I used to provide solutions to differnet problems in CS domain."
+    }
+  ],
+  "skillsDescription": "My Skills",
+  "skills": [
+    {
+      "skillname": "DSA"
+    },
+    {
+      "skillname": "Cpp"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "JavaScript"
+    },
+    {
+      "skillname": "Java"
+    }
+
+  ],
+  "portfolio": [
+    {
+      "name": "E-commerce website",
+      "description": "Website",
+      "imgurl": "images/ecommerce.jpg"
+    },
+    {
+      "name": "Portfolio Website",
+      "description": "Website",
+      "imgurl": "images/port.png"
+    },
+    {
+      "name": "Gold Price Prediction",
+      "description": "ML model",
+      "imgurl": "images/aiml.jpg"
+    },
+    {
+      "name": "Food Ordering Website",
+      "description": "Website",
+      "imgurl": "images/food.jpg"
+    }
+  ],
   
-  export default resumeData
+}
+
+export default resumeData
